14bbf6108a2491442bd73489ae917efd91f91fa4
shahista.inamdar@iris.co.uk <shahista.inamdar@iris.co.uk>
2017-10-08 19:39:50 +0530

    done with tree structure but few problem with positioning

--- index.js
+++ index.js
@@ -7,16 +7,71 @@ this.initiArray = [];
 this.arrayToTraverse = [];
 this.removedList = [];
 
-var draw = function(arrayToTraverse){debugger
+// function setPositionForParents(levelToSet){debugger
+//     if(arrayToTraverse[i].parent1 !== -1){
+//              // that.setPositionForParents($("#"+arrayToTraverse[i].character+i), arrayToTraverse.filter(function (el) {return el.level = level + 1}));
+//    // ---------------------
+//       var node = $("#"+arrayToTraverse[i].character+i);
+//       var position = node.position();
+//       var y = position.top;
+//       var x = position.left;
+
+//       var parent1Index = arrayToTraverse.findIndex(el => el.id == arrayToTraverse[i].parent1);
+//       var parent2Index = arrayToTraverse.findIndex(el => el.id == arrayToTraverse[i].parent2);
+//       if(parent1Index !== -1){
+//         arrayToTraverse[parent1Index].left = x - 5;
+//         arrayToTraverse[parent1Index].top = y - 5;
+//         arrayToTraverse[parent2Index].left = x + 5;
+//         arrayToTraverse[parent2Index].top = y + 5;
+//       }
+//     // ----------------------------------------------
+//   }
+// }
+function setPositionForParents(arrayToTraverse, levelToSet){debugger
+  var parentArray = arrayToTraverse.filter(function(el) { return el.level === levelToSet });
+  for(var i = 0; i < parentArray.length; i++){
+    if(parentArray[i].parent1 !== -1){
+             // that.setPositionForParents($("#"+parentArray[i].character+i), parentArray.filter(function (el) {return el.level = level + 1}));
+   // ---------------------
+      var node = $("#"+parentArray[i].character+parentArray[i].frequency);
+      var position = node.position();
+      var y = position.top;
+      var x = position.left;
+
+      var parent1Index = arrayToTraverse.findIndex(el => el.id == parentArray[i].parent1);
+      var parent2Index = arrayToTraverse.findIndex(el => el.id == parentArray[i].parent2);
+
+      if(parent1Index !== -1){
+        $("#"+arrayToTraverse[parent1Index].character+parentArray[i].frequency).css("left", x - 5);
+        $("#"+arrayToTraverse[parent1Index].character+parentArray[i].frequency).css("top", y - 5);
+        $("#"+arrayToTraverse[parent2Index].character+parentArray[i].frequency).css("left", x + 5);
+        $("#"+arrayToTraverse[parent2Index].character+parentArray[i].frequency).css("top", y + 5);
+      }
+    // ----------------------------------------------
+   } 
+  }
+}
+var draw = function(arrayToTraverse){
+  var that = this;
   var level;
+  var oldLevel;
+  var count = 0;
   for (var i = arrayToTraverse.length-1; i>= 0; i--){
-        if(arrayToTraverse[i].level !== level){
-          $( "<div id=level"+arrayToTraverse[i].level+" class='nextLevel'>").appendTo(".graph");
-          // $( "<div id="+arrayToTraverse[i].character+i+" class='numberCircle'>"+arrayToTraverse[i].character+"</p>" ).appendTo(".graph #"+arrayToTraverse[i].character+i+"");
-          level = arrayToTraverse[i].level;
+        if(arrayToTraverse[i].frequency !== level){
+          $( "<div id=level"+arrayToTraverse[i].frequency+" class='nextLevel'>").appendTo(".graph");
+          level = arrayToTraverse[i].frequency;
+          if(oldLevel !== level && count > 1){
+            setPositionForParents(arrayToTraverse, arrayToTraverse[i].level + 2);
+          }
+          oldLevel = level;
+          count += 1;
         }
-        $( "<div id="+arrayToTraverse[i].character+i+" class='numberCircle'>"+arrayToTraverse[i].character+"</p>" ).appendTo("#level"+level);
-      
+        $( "<div id="+arrayToTraverse[i].character+arrayToTraverse[i].frequency+" class='numberCircle'>"+"F: "+arrayToTraverse[i].frequency+"C: "+arrayToTraverse[i].character+"</p>" ).appendTo("#level"+level);
+        
+        // if(arrayToTraverse[i].top || arrayToTraverse[i].left){
+        //   $("#"+ arrayToTraverse[i].character+arrayToTraverse[i].frequency).css("top", arrayToTraverse[i].top);
+        //   $("#"+ arrayToTraverse[i].character+arrayToTraverse[i].frequency).css("left", arrayToTraverse[i].left);
+        // }
   }
 }
 
@@ -28,7 +83,9 @@ for (var i = 0, len = this.inputString.length; i < len; i++) {
     "id" : guid(),
     "character" : this.inputString[i],
     "frequency" : (this.inputString.split(this.inputString[i]).length - 1),
-    "level" : 0
+    "level" : 0,
+    "left": 0,
+    "top": 0
   };
 
   var found = false;
@@ -57,9 +114,9 @@ while (this.initiArray.length > 1) {
   this.initiArray = sortArray(this.initiArray);
 }
 
-for (var i = 0; i < this.initiArray.length; i++) {
-  console.log("Character : " + this.initiArray[i].character + "\tFrequency : " + this.initiArray[i].frequency);
-}
+// for (var i = 0; i < this.initiArray.length; i++) {
+//   console.log("Character : " + this.initiArray[i].character + "\tFrequency : " + this.initiArray[i].frequency);
+// }
 
 this.removedList.push(this.initiArray[0]);
 
@@ -71,7 +128,7 @@ var currentSize = 0;
 var that = this;
 for (var i = 0; i < this.arrayToTraverse.length; i++) {
   var binary = getBinaryFor(this.arrayToTraverse[i].character, that);
-  console.log("Character : " + this.arrayToTraverse[i].character + "\t encoding : " + binary);
+  // console.log("Character : " + this.arrayToTraverse[i].character + "\t encoding : " + binary);
   currentSize = currentSize + binary.length * that.arrayToTraverse[i].frequency;
 }
 
@@ -81,9 +138,9 @@ $("#totalSize").text((this.inputString.length * 8) + " bits");
 $("#compressedSize").text(currentSize + " bits");
 $("#PercentageCompression").text(currentSize + (100 - (Math.round((currentSize/(this.inputString.length * 8))*100 * 100) / 100)) + "%");
 
-for (var i = 0; i < this.removedList.length; i++) {
-  console.log("Character : " + this.removedList[i].character + "\t Level : " + this.removedList[i].level);
-}
+// for (var i = 0; i < this.removedList.length; i++) {
+//   console.log("Character : " + this.removedList[i].character + "\t Level : " + this.removedList[i].level);
+// }
 // console.log("Total Size Requirement : " + (this.inputString.length * 8) + " bits");
 // console.log("Compressed Size : " + currentSize + " bits");
 // console.log("Percentage Compression : " + (100 - (Math.round((currentSize/(this.inputString.length * 8))*100 * 100) / 100)) + "%");
@@ -155,7 +212,9 @@ function getNewObject(that){
     "character" : obj1.character + "" + obj2.character,
     "frequency" : obj1.frequency + obj2.frequency,
     "id" : guid(),
-    "level" : max
+    "level" : max,
+    "left": 0,
+    "top": 0
   };
 
   that.removedList.push(obj1);
